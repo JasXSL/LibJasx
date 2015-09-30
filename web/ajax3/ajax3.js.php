2fce4e347501da3b0bddb41da6c38ab4a5d077b0
JasX <jasxsecondlife@gmail.com>
2015-09-30 17:09:04 +0200

    Update ajax3
    - CSRF token can now be supplied as $_GET['CSRF_TOKEN']
    - Added an overrideErrors argument which is a function that receives
    Array errors, Array Notices as arguments
    - Added checking if the browser supports formdata
    - Added Ajax.bindErrorOverride to bind a function to override default
    error/notice behavior

--- web/ajax3/ajax3.js.php
+++ web/ajax3/ajax3.js.php
@@ -1,22 +1,32 @@
 <?php
 	header('Content-Type: application/javascript');
-	session_start();
-	if(!isset($_SESSION['CSRF_TOKEN']))$_SESSION['CSRF_TOKEN'] = md5(rand(0, 0xFFFFFFF));
+	
+	$CSRF = '';
+	if(isset($_GET['CSRF_TOKEN']))$CSRF = $_GET['CSRF_TOKEN'];
+	
 ?>
 // JavaScript Document
 // function callback((obj)this){}
-function Ajax(task, data, form, callback){
+function Ajax(task, data, form, callback, overrideErrors){
 	this.task = task;
     this.data = data;
 	this.form = form;
     this.success = false;
 	this.response = null;
+    this.url = Ajax.url;
+    this.overrideErrors = overrideErrors;
+    
+    if(window.FormData === undefined)console.log("Browser does not support file uploads.");
+    else{
+    	if(!this.form instanceof FormData)
+    		this.form = new FormData($(this.form)[0]);
+    }
 	
+    
 	var me = this;
 	
-    console.log(Ajax.url+"?t="+encodeURIComponent(me.task)+"&d="+encodeURIComponent(JSON.stringify(data))<?php if(isset($_SESSION['CSRF_TOKEN']))echo "+'&csrf=".htmlspecialchars($_SESSION['CSRF_TOKEN'])."'";?>);
     $.ajax({
-		url:Ajax.url+"?t="+encodeURIComponent(me.task)+"&d="+encodeURIComponent(JSON.stringify(data))<?php if(isset($_SESSION['CSRF_TOKEN']))echo "+'&csrf=".htmlspecialchars($_SESSION['CSRF_TOKEN'])."'";?>,
+		url:Ajax.url+"?t="+encodeURIComponent(me.task)+"&d="+encodeURIComponent(JSON.stringify(data))<?php if(!empty($CSRF))echo "+'&csrf=".urlencode($CSRF)."'";?>,
 		data:form,
         cache:false,
         contentType:false,
@@ -27,14 +37,23 @@ function Ajax(task, data, form, callback){
         //console.log(d);
 		if(d.hasOwnProperty("succ")){
         	//console.log(d);
-            if(d.err.length)addErrors(d.err.join('<br />'), true);
-			if(d.note.length)addErrors(d.note.join('<br />'), false);
-			if(d.err.length || d.note.length)$("html, body").animate({ scrollTop: 0 }, "slow");
-			if(d.redir)window.location = d.redir;
+            
+            
+            if(Ajax.overrideAllErrors !== null)Ajax.overrideAllErrors(d.err, d.note);
+            else if(me.overrideErrors !== undefined){
+            	overrideErrors(d.err, d.note);
+            }
+            else{
+               	if(d.err.length)addErrors(d.err.join('<br />'), true);
+                if(d.note.length)addErrors(d.note.join('<br />'), false);
+                if(d.err.length || d.note.length)$("html, body").animate({ scrollTop: 0 }, "slow");
+			}
+            
+            if(d.redir)window.location = d.redir;
             
             me.response = d.vars;
 			if(d.succ)me.success = true;
-		}else console.log("Ajax failure", d);
+		}else console.log("Ajax failure", me);
         if(me !== undefined && callback !== undefined)callback(me);
         for(var i in Ajax.onSuccessFunctions)Ajax.onSuccessFunctions[i](me);
 	}).fail(function(data){
@@ -47,6 +66,8 @@ function Ajax(task, data, form, callback){
 // These let you tie project specific actions
 Ajax.url = '/ajax.php';
 Ajax.onSuccessFunctions = new Array;
+Ajax.overrideAllErrors = null;
+
 Ajax.setConf = function(url, onSuccessFunctions){
 	Ajax.url = url;
     if(onSuccessFunctions !== undefined)Ajax.onSuccessFunctions = onSuccessFunctions;
@@ -54,4 +75,12 @@ Ajax.setConf = function(url, onSuccessFunctions){
 Ajax.bindOnSuccessFunction = function(fn){
 	Ajax.onSuccessFunctions.push(fn);
 }
+Ajax.bindErrorOverride = function(fn){
+	Ajax.overrideAllErrors = fn;
+}
 
+<?php
+
+	session_write_close();
+
+?>
